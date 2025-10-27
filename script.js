9dc9840a520d0f9cb04d4b0bf77020e28d9d0e2f
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-27 20:11:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,22 @@
 const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
-const buttons = document.getElementById("buttons");
 
-// Add sound buttons
-sounds.forEach((sound) => {
-  const btn = document.querySelector(`button:contains('${sound}')`);
-  if (btn) {
-    btn.addEventListener("click", () => {
-      stopSounds();
-      const audio = new Audio(`sounds/${sound}.mp3`);
-      audio.play();
-      audio.id = "playingSound";
-    });
-  }
+sounds.forEach(sound => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+  btn.textContent = sound;
+
+  btn.addEventListener("click", () => {
+    stopSounds();
+    document.getElementById(sound).play();
+  });
+
+  document.body.appendChild(btn);
 });
 
-// Stop button functionality
-document.querySelector(".stop").addEventListener("click", stopSounds);
-
 function stopSounds() {
-  const playing = document.getElementById("playingSound");
-  if (playing) {
-    playing.pause();
-    playing.currentTime = 0;
-    playing.remove();
-  }
+  sounds.forEach(sound => {
+    const audio = document.getElementById(sound);
+    audio.pause();
+    audio.currentTime = 0;
+  });
 }
-
