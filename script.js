b888109dde63399bc98b09fb4612a34ba2d72e41
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-27 20:04:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const buttons = document.getElementById("buttons");
+
+// Add sound buttons
+sounds.forEach((sound) => {
+  const btn = document.querySelector(`button:contains('${sound}')`);
+  if (btn) {
+    btn.addEventListener("click", () => {
+      stopSounds();
+      const audio = new Audio(`sounds/${sound}.mp3`);
+      audio.play();
+      audio.id = "playingSound";
+    });
+  }
+});
+
+// Stop button functionality
+document.querySelector(".stop").addEventListener("click", stopSounds);
+
+function stopSounds() {
+  const playing = document.getElementById("playingSound");
+  if (playing) {
+    playing.pause();
+    playing.currentTime = 0;
+    playing.remove();
+  }
+}
+
